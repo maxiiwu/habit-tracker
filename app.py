0110ada073df72ec4769171b9ca06e997b483c82
Maxima <maximawu@gmail.com>
2026-08-17 14:50:37 -0300

    Limpieza de archivos del tutorial y CRUD propio terminado

--- app.py
+++ app.py
@@ -1,41 +1,87 @@
-from datetime import date
-from flask import Flask, render_template  
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# Esta linea importa las herramientas necesarias de la libreria flask que isntalamos
-# Flask: es el q conveirte el archivo Python en un servidor de web
-# render_template: es una funcion especial de Flask q se encarga de tomar un archivo HTML y lo envia al navegador del usuario
+from flask import Flask, render_template, request, redirect, url_for
 
+# 1. Crear la aplicacion
 app = Flask(__name__)
-# Crea la aplicacion web y la guarda dentro de la variable app
-# __name__: es una variable especial de Python. Le indica a Flask donde esta ubicado este archivo dentro de tu proyecto para q sepa dodne buscar las imagenes, estilos y paginas HTML cuando las necesite
+# Hace q el archivo Python se convierta en un servidor web q se peuda ver en el navegador
 
+# 2. Le decimos a Flask donde se va a crear la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///habits.db'
-# Le estamos diciendo al Flask q a partir de ahora, todos los habitos q el usuario cree, modifique o borre, los va a ir guardando de forma permanente adentro de un archivo llamado habits.db
+# habits.db es como un archivo de Excel gigante
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'dev-password-secret-key'
 
+# 3. Conectar la base de datos a la app
 db = SQLAlchemy(app)
 
+# Estamos creando un Modelo. Le estamos ensenando a la base de datos que significa exactamente un "Habito" para nosotoras
+class Habito(db.Model):  # asi es como le decimos a Python como disenar las columnas d esa tabla Excel
+    id = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(100), nullable = False)
+    completado = db.Column(db.Boolean, default = False)
+# id: nro unico para q la base de datos no confunda "Tomar agua" de la manana con "Tomar agua" de la tarde
+# nombre: el texto en si
+# estado: interruptor para saber si ya lo completaste hoy
+# db.Column == "database Column" --> le avisa a SQLAlchemy que la variable q estamos definiendo (id, nombre, completado) va a ser una columna dentro de nuestra tabla
+# db.Integer: configuramos para q esa celda acepte solo nros enteros
+# db.String: configuramos para q esa celde acepte solo string (con 100 siendo el limite max de caracteres permitidos)
+# db.Boolean: configuramos para q sea solo un bool. El habito puede estar: completado (completado = True) o pendiente (completado = False)
+# primary_key = True: le dice a Python q no se repitan las cosas. Asiq si tenemos un habito "Leer" y despues otro habit "Leer", la base de datos va a saber q son diferentes
+# nullable = False: le prohibe a la base de datos guardar un habito donde no tenia nada escrito
+# default = False: si agregas un habito nuevo y te olvidas de avisarle si ya fue completado, automaticamente asume q todavia no lo hiciste
+
 with app.app_context():
-    db.create_all() 
+    db.create_all()
 
 
+@app.route("/")
+def inicio():
+    lista_habitos = Habito.query.all()
+    return render_template("index.html", habitos = lista_habitos)
 
-@app.route('/')
-# Crea una ruta (una pagina web)
-def index():
-    return render_template('index.html')
+@app.route("/agregar", methods = ["POST"])  # Le decimos a Flask, q esta ruta solo acepta envio de datos (POST)
+def agregar_habito():
+    nombre_nuevo = request.form.get("nombre_habito")  
+    # nombre_habito es el name q le pusimos al input
 
-# Ruta de los Hábitos
-@app.route("/habits")
-def habits():
-    return render_template("habits.html")
+    nuevo_habito = Habito(nombre = nombre_nuevo)
+    # Creamos un nuevo objeto Habito con ese nombre
 
-# Ruta de las Estadísticas
-@app.route("/analytics")
-def analytics():
-    return render_template("analytics.html")
+    db.session.add(nuevo_habito)
+    db.session.commit()  # Es el equivalente a hacer "Guardar archivo" en un Excel
+    # Lo agregamos a la base de datos y guardamos los cambios
 
+    return redirect(url_for("inicio"))
+    # Lo redireccionamos a la pagina ppal para ver la lista actualizada
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/completar/<int:id>")
+def completar_habito(id):
+    habito_a_modificar = Habito.query.get(id)
+    # Buscamos en la tabla de la base de datos el habito q tenga ese ID
+
+    habito_a_modificar.completado = not habito_a_modificar.completado
+    # Le cambiamos el estado de completacion al opuesto
+
+    db.session.commit()
+    # Guardamos los cambios en nuestro archivo de base de datos
+
+    return redirect(url_for("inicio"))
+    # url_for: direccion de web para "inicio"
+    # redirect: redirecciona
+    # Estás en la página principal (/) viendo tus hábitos.Haces clic en el botón y Chrome viaja hacia la ruta trasera de Flask (por ejemplo, /completar/1).
+    # En esa ruta trasera, Python hace su magia: busca el hábito, le cambia el estado a Verdadero y lo guarda en la base de datos. ¡Pero esa ruta no tiene un archivo HTML o diseño visual para mostrarte!
+    # Si nosotros no ponemos nada al final, la página se quedaría en blanco o daría error porque no sabe qué dibujarle al usuario.
+    # Al poner return redirect(url_for("inicio")), le decimos: "Apenas termines de tachar y guardar el hábito en el Excel de fondo, agarrá al usuario y mandalo de regreso a la página principal a la velocidad de la luz".
+
+@app.route("/eliminar/<int:id>")
+def eliminar_habito(id):
+    habito_a_eliminar = Habito.query.get(id)
+
+    db.session.delete(habito_a_eliminar)
+
+    db.session.commit()
+
+    return redirect(url_for("inicio"))
+
+if __name__ == "__main__":
+    app.run(debug = True)
